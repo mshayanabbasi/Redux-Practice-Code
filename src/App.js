e93e80fc327f5c75a6d32389be1d78a193253097
mshayanabbasi <mshayanabbasi>
2019-07-13 20:52:47 +0500

    redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-
+          Hello World
       </div>
     )
   }
