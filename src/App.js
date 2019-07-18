cd94916ef445763b4a19bf3535a0a7ba09d3984c
mshayanabbasi <mshayanabbasi>
2019-07-18 17:28:19 +0500

    redux

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import {connect} from "react-redux"
 import store from './basic'
 import './App.css';
+import TodoAction from './store/Action/todoAction';
 
-function UpdateTodo(){
-  return {
-      type: "ADD_TODO"
-  }
-}
+// function UpdateTodo(){
+//   return {
+//       type: "ADD_TODO"
+//   }                                    
+// }
 
 class App extends React.Component {
   componentDidMount() {
-    console.log(this.props);
-    this.props.UpdateTodo()
+    // console.log(this.props);
+    // this.props.UpdateTodo({name: "Shayan", password:"123"})
+    this.props.getTodo()
   }
   componentWillReceiveProps(nextProps) {
     console.log("componentWillReceiveProps", nextProps);
@@ -36,8 +38,12 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    UpdateTodo: (data) => dispatch(UpdateTodo(data))
+    // updateTodo: (data) => dispatch(UpdateTodo(data))
+    getTodo: (data) => dispatch(TodoAction.getTodo(data))
   }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(App);
+
+
+// export default App
